2871df048561aec8f8cd5903289a3674e089c549
Your Name <zibriyal.shaikh@example.com>
2025-10-02 20:00:51 +0530

    Div() added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -13,11 +13,17 @@ public class MyCalc {
 	{
 		return a*b;
 	}
+	public int div (int a , int b)
+	{
+		return a/b;
+	}
 	public static void main (String[] args) {
 		MyCalc Calc = new MyCalc();
 		System.out.println("sum is " + Calc.sum(20,10));
 		System.out.println("Diff is " + Calc.diff(20,10));
 		System.out.println("Mul is " + Calc.mul(20,10));
+		System.out.println("Div is " + Calc.div(20,10));
+
 
 
 	}
