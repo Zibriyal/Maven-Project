4c2c1088e97b3c72bf0c26a4c451c82ec09b1d01
Your Name <zibriyal.shaikh@example.com>
2025-10-02 19:49:15 +0530

    Mul() added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -9,11 +9,16 @@ public class MyCalc {
 	{
 		return a-b;
 	}
-	
+	public int mul (int a , int b)
+	{
+		return a*b;
+	}
 	public static void main (String[] args) {
 		MyCalc Calc = new MyCalc();
 		System.out.println("sum is " + Calc.sum(20,10));
 		System.out.println("Diff is " + Calc.diff(20,10));
+		System.out.println("Mul is " + Calc.mul(20,10));
+
 
 	}
 
